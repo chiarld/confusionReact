8624332a8651d12fb0d73764154ece21d330e68c
Dan Chiarlone <danilochiarlone@gmail.com>
2018-10-04 20:26:46 -0700

    Functional components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,72 +1,66 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class DishDetail extends Component
+function RenderDish({dish})
 {
-    renderDish(dish)
+    return(
+        <Card>
+            <CardImg width ="100%" src={dish.image} alt={dish.name}/>
+            <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+}
+function RenderComments({comments})
+{   
+    return(
+        comments.map((comment) => {
+            return(
+                <div key={comment.id}>
+                    <li>{comment.comment}</li>
+                    <br></br>
+                    <li> -- {comment.author}, 
+                        {Intl.DateTimeFormat('en-US', {
+                        year: 'numeric',
+                        day: '2-digit',
+                        month: 'short'
+                        }).format(new Date(comment.date))} 
+                    </li>
+                    <br></br>
+                </div>
+            );
+        })
+    );
+}
+const DishDetail = (props) => {
+    if(props.dish == null)
     {
         return(
-            <Card>
-                <CardImg width ="100%" src={dish.image} alt={dish.name}/>
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
+            <div>
+            </div>
         );
     }
-
-    renderComments()
-    {   
+    else
+    {
         return(
-            this.props.dish.comments.map((comment) => {
-                return(
-                    <div key={comment.id}>
-                        <li>{comment.comment}</li>
-                        <br></br>
-                        <li> -- {comment.author}, 
-                            {Intl.DateTimeFormat('en-US', {
-                            year: 'numeric',
-                            day: '2-digit',
-                            month: 'short'
-                            }).format(new Date(comment.date))} 
-                        </li>
-                        <br></br>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish = {props.dish}/>
                     </div>
-                );
-            })
+                    <div className="col-12 col-md-5 m-1">
+                        <h4>Comments</h4>
+                        <ul className="list-unstyled">
+                            <RenderComments comments={props.dish.comments} />
+                        </ul>
+                    </div>
+                </div> 
+            </div>
         );
-    }
-
-    render()
-    {
-        if(this.props.dish == null)
-        {
-            return(
-                <div>
-
-                </div>
-            );
-        }
-        else
-        {
-            return(
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12 col-md-5 m-1">
-                            {this.renderDish(this.props.dish)}
-                        </div>
-                        <div className="col-12 col-md-5 m-1">
-                            <h4>Comments</h4>
-                            <ul className="list-unstyled">
-                                {this.renderComments(this.props.dish)}
-                            </ul>
-                        </div>
-                    </div> 
-                </div>
-            );
-        }
     }
 }
+
 
 export default DishDetail;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,31 +1,34 @@
-import React, {Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-class Menu extends Component 
+function RenderMenuItem({ dish, onClick })
 {
-    render()
-    {
-        const menu = this.props.dishes.map((dish) => { // foreach(var dish in dishes){...}
-            return (
-                <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card onClick={() => this.props.onClick(dish.id)}>
-                        <CardImg width="100%" src={dish.image} alt={dish.name}/>
-                        <CardImgOverlay>
-                            <CardTitle>{dish.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
-                </div> // key is necessary because we are creating a list of items and that's how react handles it
-            );
-        }); // iterates over the dish array and maps into schema
+    return(
+        <Card onClick={() => onClick(dish.id)}>
+            <CardImg width="100%" src={dish.image} alt={dish.name}/>
+            <CardImgOverlay>
+                <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+        </Card>
+    );
+}
 
+const Menu = (props) => {
+    const menu = props.dishes.map((dish) => { // foreach(var dish in dishes){...}
         return (
-            <div className="container">
-                <div className="row">
-                    {menu}
-                </div>
-            </div>
+            <div key={dish.id} className="col-12 col-md-5 m-1">
+                <RenderMenuItem dish={dish} 
+                    onClick = {props.onClick}/>
+            </div> // key is necessary because we are creating a list of items and that's how react handles it
         );
-    }
+    }); // iterates over the dish array and maps into schem
+    return (
+        <div className="container">
+            <div className="row">
+                {menu}
+            </div>
+        </div>
+    );
 }
 
 export default Menu;
