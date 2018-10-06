ac3d51c256c4cf33a69519e1d46c52033d2d328a
Dan Chiarlone <danilochiarlone@gmail.com>
2018-10-06 16:31:19 -0700

    Single Page Applications Part 2

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderMenuItem({ dish, onClick })
 {
     return(
-        <Card onClick={() => onClick(dish.id)}>
-            <CardImg width="100%" src={dish.image} alt={dish.name}/>
-            <CardImgOverlay>
-                <CardTitle>{dish.name}</CardTitle>
-            </CardImgOverlay>
+        <Card>
+            <Link to={`menu/${dish.id}`}>
+                <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
         </Card>
     );
 }
@@ -17,13 +20,22 @@ const Menu = (props) => {
     const menu = props.dishes.map((dish) => { // foreach(var dish in dishes){...}
         return (
             <div key={dish.id} className="col-12 col-md-5 m-1">
-                <RenderMenuItem dish={dish} 
-                    onClick = {props.onClick}/>
+                <RenderMenuItem dish={dish}/>
             </div> // key is necessary because we are creating a list of items and that's how react handles it
         );
     }); // iterates over the dish array and maps into schem
     return (
         <div className="container">
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>Menu</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>Menu</h3>
+                    <hr/>
+                </div>
+            </div>
             <div className="row">
                 {menu}
             </div>
