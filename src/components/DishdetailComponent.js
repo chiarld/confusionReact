e7092d32c70d6a22f0f88816e75c107290e217a6
Dan Chiarlone <danilochiarlone@gmail.com>
2018-11-09 11:52:03 -0800

    Fetch from server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import { Label, Row, Col, Card, CardImg, CardText, CardBody, CardTitle, Breadcru
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
 import { Control, LocalForm, Errors} from 'react-redux-form';
+import { baseUrl } from '../shared/baseUrl';
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => (val) && (val.length >= len);
@@ -11,7 +12,7 @@ function RenderDish({dish})
 {
     return(
         <Card>
-            <CardImg width ="100%" src={dish.image} alt={dish.name}/>
+            <CardImg width ="100%" src={baseUrl + dish.image} alt={dish.name}/>
             <CardBody>
                 <CardTitle>{dish.name}</CardTitle>
                 <CardText>{dish.description}</CardText>
--- src/redux/promotions.js
+++ src/redux/promotions.js
@@ -1,8 +1,21 @@
-import { PROMOTIONS } from '../shared/promotions';
+import * as ActionTypes from './ActionTypes';
 
-export const Promotions = (state = PROMOTIONS, action) =>
-{
+export const Promotions = (state = {
+    isLoading: true, //true because dishes is an empty array
+    errMess: null, //if fetching didn't work
+    promotions: [] //filled by addDishes, action) =>
+}, action) => {
     switch(action.type){
+
+        case ActionTypes.ADD_PROMOS:
+            return{...state, isLoading:false, errMess: null, promotions: action.payload} // still immutable
+
+        case ActionTypes.PROMOS_LOADING:
+            return{...state, isLoading:true, errMess: null, promotions: []} // still immutable
+            
+        case ActionTypes.PROMOS_FAILED:
+            return{...state, isLoading:false, errMess: action.payload} // still immutable
+                 
         default:
             return state;
     }
